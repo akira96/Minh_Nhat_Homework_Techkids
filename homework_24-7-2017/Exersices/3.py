78467fe803869bfd68d36e613fd975daa1dc9a4b
HOANGNAM\mrnha_000 <mrnhat08051996@gmail.com>
2017-07-27 18:11:39 +0700

    no message

---
+++ homework_24-7-2017/Exersices/3.py
@@ -0,0 +1,21 @@
+for i in range(40):
+    print('*', end="") 
+print()
+
+
+
+for i in range(20):
+    print("x*", end="")
+print()
+
+
+print()
+
+
+for i in range(5):
+    for i in range(5):
+        print("x*", end="")
+    print()
+    for i in range(5):
+        print("*x", end="")
+    print()
---
+++ homework_24-7-2017/Study/vd.py
@@ -0,0 +1,10 @@
+t = float(input("Temperature?")
+
+if (t >= 27):
+    if (t >= 50):
+        print("So hot")
+    else:
+        print("Moderate")
+else:
+    print("Cold")
+    
