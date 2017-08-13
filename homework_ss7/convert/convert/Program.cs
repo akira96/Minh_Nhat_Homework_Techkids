6b02b2c6062d064ef38085d692cd33eaf12bb618
HOANGNAM\mrnha_000 <mrnhat08051996@gmail.com>
2017-08-13 23:00:01 +0700

    no message

---
+++ homework_ss7/convert/convert/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace convert
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int n, i, j, bin = 0, dec;
+			Console.WriteLine ("Chuyen doi thap phan thanh nhi phan trong C#");
+			Console.WriteLine ("--------------------------------------------");
+			Console.Write ("Nhap mot so thap phan bat ky: ");
+			n = Convert.ToInt32 (Console.ReadLine());
+			dec = n;
+			i = 1;
+			for (j = n; j > 0; j = j / 2)
+			{
+				bin = bin + (n % 2) * i;
+				i = i * 10;
+				n = n / 2;
+			}
+			Console.WriteLine ("So trong he nhi phan tuong duong cua so {0} la {1}", dec, bin);
+		}
+	}
+}
